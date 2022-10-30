b6c129d6a2748b30a929074f43f66052140b4bf2
Raheem Salifu Kasim <r.kasim@aol.com>
2022-10-30 22:51:17 +0000

    Task Update

--- models/base_model.py
+++ models/base_model.py
@@ -56,8 +56,12 @@ class BaseModel:
 
     def to_dict(self):
         """Convert instance into dict format"""
-	aux_dict = self.__dict__.copy()
-        aux_dict['__class__'] = self.__class__.__name__
-        aux_dict['created_at'] = self.created_at.isoformat()
-        aux_dict['updated_at'] = self.updated_at.isoformat()
-        return aux_dict
+	new = {}
+        var = self.__dict__
+        for elem in var:
+            if elem == 'created_at' or elem == 'updated_at':
+                new[elem] = var[elem].isoformat()
+            else:
+                new[elem] = var[elem]
+        new['__class__'] = self.__class__.__name__
+        return (new)
