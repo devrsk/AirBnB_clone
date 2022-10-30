2a0b40d2557c6cfbcefb9db7eb8191a0239890ec
Raheem Salifu Kasim <r.kasim@aol.com>
2022-10-30 22:01:11 +0000

    Task Update

--- models/base_model.py
+++ models/base_model.py
@@ -1,67 +1,51 @@
-#!/usr/bin/python3
-"""This module defines a base class for all models in our hbnb clone"""
-import os
+#!usr/bin/python3
+"""Definition of the BaseModel class."""
 import uuid
 from datetime import datetime
-from sqlalchemy import Column, String, DATETIME
-from sqlalchemy.ext.declarative import declarative_base
-
-
-Base = declarative_base()
+import models
 
 
 class BaseModel:
-    """A base class for all hbnb models"""
-    id = Column(String(60), nullable=False, primary_key=True, unique=True)
-    created_at = Column(DATETIME, nullable=False, default=datetime.utcnow())
-    updated_at = Column(DATETIME, nullable=False, default=datetime.utcnow())
+    """Defines all common attributes/methods for other classes."""
 
     def __init__(self, *args, **kwargs):
-        """Instantiates a new model"""
-        if not kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-        else:
+        """Initialize the BaseModel class.
+        Args:
+            self (BaseModel): the current instance
+            args (any): not used here
+            kwargs (dict): dictionary of key/value pairs attributes
+        """
+        # public instance attributes
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+        if len(kwargs):
+            iso_format = "%Y-%m-%dT%H:%M:%S.%f"
             for key, value in kwargs.items():
-                if key != '__class__':
-                    if key in ('created_at', 'updated_at'):
-                        setattr(self, key, datetime.fromisoformat(value))
-                    else:
-                        setattr(self, key, value)
-            # if os.getenv('HBNB_TYPE_STORAGE') in ('db'):
-            if not hasattr(kwargs, 'id'):
-                setattr(self, 'id', str(uuid.uuid4()))
-            if not hasattr(kwargs, 'created_at'):
-                setattr(self, 'created_at', datetime.now())
-            if not hasattr(kwargs, 'updated_at'):
-                setattr(self, 'updated_at', datetime.now())
+                if key in ["created_at", "updated_at"]:
+                    self.__dict__[key] = datetime.strptime(value, iso_format)
+                else:
+                    self.__dict__[key] = value
+        else:
+            models.storage.new(self)
 
-    def __str__(self):
-        """Returns a string representation of the instance"""
-        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
-
-    def delete(self):
-        """Deletes this BaseModel instance from the storage"""
-        from models import storage
-        storage.delete(self)
-
+    # public instance methods
     def save(self):
-        """Updates updated_at with current time when instance is changed"""
-        from models import storage
-        self.updated_at = datetime.now()
-        storage.new(self)
-        storage.save()
+        """Updates the public instance attribute updated_at \
+            with the current datetime."""
+        self.updated_at = datetime.today()
+        models.storage.save()
 
     def to_dict(self):
-        """Convert instance into dict format"""
-        res = {}
-        for key, value in self.__dict__.items():
-            if key != '_sa_instance_state':
-                if isinstance(value, datetime):
-                    res[key] = value.isoformat()
-                else:
-                    res[key] = value
-        res['__class__'] = self.__class__.__name__
-        return res
+        """Returns a dictionary containing all \
+            keys/values of __dict__ of the instance."""
+        dict_copy = self.__dict__.copy()
+        dict_copy["created_at"] = self.created_at.isoformat()
+        dict_copy["updated_at"] = self.updated_at.isoformat()
+        dict_copy["__class__"] = self.__class__.__name__
+
+        return (dict_copy)
+
+    def __str__(self):
+        """Return the string representation of the instance."""
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
