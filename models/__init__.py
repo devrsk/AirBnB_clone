da8e151c7acd149a6bbfdb81f1ca1e875bfdad91
Raheem Salifu Kasim <r.kasim@aol.com>
2022-10-30 23:27:18 +0000

    Task Update

--- models/__init__.py
+++ models/__init__.py
@@ -1,10 +1,16 @@
-import os
-
-from models.engine.db_storage import DBStorage
-from models.engine.file_storage import FileStorage
-
-storage = DBStorage() if os.getenv(
-    'HBNB_TYPE_STORAGE') == 'db' else FileStorage()
-"""A unique FileStorage/DBStorage instance for all models.
+#!/usr/bin/python3
 """
+initialize the models package
+"""
+
+from os import getenv
+
+# DBSTORAGE
+if getenv('HBNB_TYPE_STORAGE') == 'db':
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+# FILESTORAGE
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
 storage.reload()
